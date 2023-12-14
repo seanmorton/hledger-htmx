912b6e9508d43b4c08dd92416c24db1bb5cacb7d
Sean Morton <seanmorton@hey.com>
2023-12-13 19:52:10 -0600

    Default date range

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"net/http"
+	"time"
 
 	"github.com/a-h/templ"
 	"github.com/seanmorton/hledger-htmx/internal/hledger"
@@ -14,10 +15,11 @@ var FS embed.FS
 
 func main() {
 	defaultAccount := "xp"
-	balances, _ := hledger.Balances(defaultAccount, "2023-11-15", "2023-11-30")
-	register, _ := hledger.Register(defaultAccount, "2023-11-15", "2023-11-30")
+	defaultFrom, defaultTo := defaultDateRange()
+	balances, _ := hledger.Balances(defaultAccount, defaultFrom, defaultTo)
+	register, _ := hledger.Register(defaultAccount, defaultFrom, defaultTo)
 
-	index := templates.Index(defaultAccount, balances, register)
+	index := templates.Index(defaultAccount, defaultFrom, defaultTo, balances, register)
 	http.Handle("/", templ.Handler(index))
 
 	http.HandleFunc("/expenses", func(w http.ResponseWriter, r *http.Request) {
@@ -34,4 +36,17 @@ func main() {
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.FS(FS))))
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func defaultDateRange() (string, string) {
+	var from, to string
+	now := time.Now()
+	if now.Day() < 16 {
+		from = time.Date(now.Year(), now.Month()-1, 15, 0, 0, 0, 0, time.Local).Format(time.DateOnly)
+		to = time.Date(now.Year(), now.Month(), 16, 0, 0, 0, 0, time.Local).Format(time.DateOnly)
+	} else {
+		from = time.Date(now.Year(), now.Month(), 15, 0, 0, 0, 0, time.Local).Format(time.DateOnly)
+		to = time.Date(now.Year(), now.Month()+1, 16, 0, 0, 0, 0, time.Local).Format(time.DateOnly)
+	}
+	return from, to
 }
