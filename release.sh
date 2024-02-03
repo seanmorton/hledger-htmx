dee98a0bbc06e03a2eb23a641d268c19141d4be8
Sean Morton <seanmorton@hey.com>
2024-02-03 16:14:48 -0600

    Always push latest on release

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker tag seanmorton/hledger-htmx:latest seanmorton/hledger-htmx:"$1" &&
+docker push seanmorton/hledger-htmx:latest &&
 docker push seanmorton/hledger-htmx:"$1" &&
 git tag "$1" &&
 git push --tags
